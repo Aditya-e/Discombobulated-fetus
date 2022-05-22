c6df588d1716a7b3a7039dbac419c6c609bdabb2
priesthood <adityagulati07@gmail.com>
2022-05-23 03:12:54 +0530

    Action Bar color changed

--- app/src/main/java/com/hfad/volume/MainActivity.java
+++ app/src/main/java/com/hfad/volume/MainActivity.java
@@ -1,11 +1,15 @@
 package com.hfad.volume;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toolbar;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -13,10 +17,20 @@ import com.hfad.volume.FragmentsFolder.VolumeFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ActionBar actionBar;
+        actionBar = getSupportActionBar();
+        ColorDrawable colorDrawable
+                = new ColorDrawable(Color.parseColor("#009FFF"));
+
+        actionBar.setBackgroundDrawable(colorDrawable);
+        actionBar.setTitle(R.string.AppTitle);
+
 //        FirebaseDatabase database=FirebaseDatabase.getInstance();
 //        DatabaseReference rootRef=database.getReference("message");
 
